8c7ba486465883b9032b165bb1359f019c702e8c
Kanishk Gupta <kanishkgupta1234@gmail.com>
2021-07-22 10:28:44 +0530

    refactor: css imports

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,5 @@
 import Navbar from "../components/layouts/Navbar";
 import '../styles/main.css';
-import 'tailwindcss/tailwind.css';
-import 'antd/dist/antd.css';
 import axios from "axios";
 import { ProductsProvider } from "../context/productContext";
 axios.defaults.baseURL='https://devbazar.herokuapp.com';
