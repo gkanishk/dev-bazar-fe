db77a6063959bc5ac9c9ef0c55e10b60b0203735
Kanishk Gupta <kanishkgupta1234@gmail.com>
2021-07-29 09:49:30 +0530

    Added page tiile

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,16 @@ import Navbar from "../components/layouts/Navbar";
 import '../styles/main.css';
 import { ProductsProvider } from "../context/productContext";
 import { UserProvider } from "../context/userContext";
+import React from "react";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
 
   return (
     <>
+      <Head>
+        <title>Dev Bazar</title>
+      </Head>
       <UserProvider>
         <ProductsProvider>
           <Navbar />
