9206712323a4611a77528c05406329c4c507e78e
Kanishk Gupta <kanishkgupta1234@gmail.com>
2021-08-23 07:55:34 +0530

    style: fixed image layout shift

--- components/common/EmptyLoginScreen.tsx
+++ components/common/EmptyLoginScreen.tsx
@@ -7,7 +7,9 @@ function EmptyLoginScreen({pageName,img}){
         <div className="grid p-4 place-items-center">
             <span className="text-lg font-medium">PLEASE LOGIN</span>
             <span className="text-gray-400 uppercase">TO VIEW ITEMS IN {pageName}</span>
+            <div className="h-80">
             <img className="mt-4 ml-4 max-h-80" src={`${img}`} alt="Login Image"/>
+            </div>
             <Link href={`/login?referer=${pageName}`}>
                 <Button className="px-10 m-8" type="primary" ghost>
                     Login
