0c244790f9f2436d5e3bcdfc284abbb6196eab51
Kanishk Gupta <kanishkgupta1234@gmail.com>
2022-12-03 11:38:19 +0530

    update BE url

--- hooks/useAxios.ts
+++ hooks/useAxios.ts
@@ -2,62 +2,62 @@ import axios, { AxiosInstance, AxiosResponse } from "axios";
 import { useEffect, useState } from "react";
 import { useUser } from "../context/userContext";
 
-let axiosClient:AxiosInstance=null;
+let axiosClient: AxiosInstance = null;
 
-const getAxiosClient=(accessToken)=>{
-  axiosClient = axios.create({
-    baseURL: 'https://devbazar.herokuapp.com',
-    headers: {
-      "Content-Type": "application/json",
-      "Authorization": `Bearer ${accessToken}`
-    }
-});
-  return axiosClient;
+const getAxiosClient = (accessToken) => {
+    axiosClient = axios.create({
+        baseURL: 'https://gkanishk-devbazar-be.vercel.app',
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${accessToken}`
+        }
+    });
+    return axiosClient;
 }
 
-export {getAxiosClient}
+export { getAxiosClient }
 
-const useAxios=(method: 'GET' | 'POST' | 'DELETE' | 'PUT',url:string,body:Record<string,any>,params:Record<string,any>)=>{
-    const [response,setResponse]=useState<AxiosResponse>(null);
-    const [error,setError]=useState<Error>(null);
-    const [isLoading,setIsLoading]=useState<boolean>(true);
-    const {accessToken} = useUser();
+const useAxios = (method: 'GET' | 'POST' | 'DELETE' | 'PUT', url: string, body: Record<string, any>, params: Record<string, any>) => {
+    const [response, setResponse] = useState<AxiosResponse>(null);
+    const [error, setError] = useState<Error>(null);
+    const [isLoading, setIsLoading] = useState<boolean>(true);
+    const { accessToken } = useUser();
 
-    useEffect(()=>{
-      (async()=>{
-        try {
-            switch(method) {
-                case 'GET':
-                    let response=await getAxiosClient(accessToken).get(url,{params})
-                    setResponse(response);
-                    setIsLoading(false);
-                    break;
-                case 'POST':
-                    response=await getAxiosClient(accessToken).post(url,body)
-                    setResponse(response);
-                    setIsLoading(false);
-                    break;
-                case 'PUT':
-                    response = await getAxiosClient(accessToken).put(url,body)
-                    setResponse(response);
-                    setIsLoading(false);
-                    break;
-                case 'DELETE':
-                    response = await getAxiosClient(accessToken).delete(url)
-                    setResponse(response);
-                    setIsLoading(false);
-                    break;
-                default:
-                    break;
+    useEffect(() => {
+        (async () => {
+            try {
+                switch (method) {
+                    case 'GET':
+                        let response = await getAxiosClient(accessToken).get(url, { params })
+                        setResponse(response);
+                        setIsLoading(false);
+                        break;
+                    case 'POST':
+                        response = await getAxiosClient(accessToken).post(url, body)
+                        setResponse(response);
+                        setIsLoading(false);
+                        break;
+                    case 'PUT':
+                        response = await getAxiosClient(accessToken).put(url, body)
+                        setResponse(response);
+                        setIsLoading(false);
+                        break;
+                    case 'DELETE':
+                        response = await getAxiosClient(accessToken).delete(url)
+                        setResponse(response);
+                        setIsLoading(false);
+                        break;
+                    default:
+                        break;
+                }
+            } catch (error) {
+                setError(error);
+                setIsLoading(false);
             }
-        }catch (error) {
-            setError(error);
-            setIsLoading(false);
-        }
-    })()
-    },[])
+        })()
+    }, [])
 
-    return {response,error,isLoading}
+    return { response, error, isLoading }
 
 }
 export default useAxios;
